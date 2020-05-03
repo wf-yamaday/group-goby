cb181bab362aa554ea742eceabc1d7c24c2b54d2
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2020-05-03 10:36:40 +0900

    feat: roomの新規作成formのコンポーネントの作成

---
+++ app/components/NewRoomForm.vue
@@ -0,0 +1,48 @@
+<template>
+  <form>
+    <v-text-field
+      v-model="newRoomForm.roomName.value"
+      :label="newRoomForm.roomName.label"
+      :loading="isLading"
+      :disabled="isLading"
+    />
+    <v-text-field
+      v-model="newRoomForm.ownerName.value"
+      :label="newRoomForm.ownerName.label"
+      :loading="isLading"
+      :disabled="isLading"
+    />
+    <v-btn color="primary" :disabled="isLading" @click="doSubmit">作成</v-btn>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isLading: false,
+      newRoomForm: {
+        roomName: {
+          value: '',
+          label: 'ルーム名'
+        },
+        ownerName: {
+          value: '',
+          label: '主催者名'
+        }
+      }
+    }
+  },
+  methods: {
+    doSubmit() {
+      this.isLading = true
+      const formData = {
+        roomName: this.newRoomForm.roomName.value,
+        ownerName: this.newRoomForm.ownerName.value
+      }
+      this.isLading = false
+      this.$emit('submit', formData)
+    }
+  }
+}
+</script>
