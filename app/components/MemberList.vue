ea752fd91664997fb3804eda105ca3d1a5133595
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2020-05-03 11:12:03 +0900

    feat: 参加者一覧を表示するコンポーネントの作成

---
+++ app/components/MemberList.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-list>
+    <v-subheader>参加者一覧</v-subheader>
+    <v-list-item v-for="user in users" :key="user.id">
+      <v-list-item-content>{{ user.name }}</v-list-item-content>
+      <v-list-item-action>
+        <v-icon v-show="user.isReady">mdi-check-circle</v-icon>
+        <p v-show="!user.isReady">waiting ...</p>
+      </v-list-item-action>
+    </v-list-item>
+  </v-list>
+</template>
+
+<script>
+export default {
+  props: {
+    users: {
+      type: Array,
+      default: () => {
+        return [
+          {
+            id: 'a-1',
+            name: 'ユーザー1',
+            isReady: false
+          },
+          {
+            id: 'a-2',
+            name: 'ユーザー2',
+            isReady: true
+          },
+          {
+            id: 'a-3',
+            name: 'ユーザー3',
+            isReady: false
+          }
+        ]
+      }
+    }
+  }
+}
+</script>
