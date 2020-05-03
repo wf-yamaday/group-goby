ce4129788becd3afc3d3dbce48e3c2385f902e4d
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2020-05-03 13:46:38 +0900

    feat: トップページに表示する説明のコンポーネントの配置

---
+++ app/components/WhatWordWolf.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-card min-width="360" light>
+    <div class="d-flex flex-no-wrap justify-space-between">
+      <div>
+        <v-img width="150" height="180" src="/wolfman.png" />
+      </div>
+      <div>
+        <v-card-title>Word Wolfとは？</v-card-title>
+        <span>
+          Word Wolfは「異なるお題」を与えられた人(Wolf)を探し出すゲームです．
+        </span>
+      </div>
+    </div>
+    <v-card-actions>
+      <v-btn color="info" outlined block>ルールの詳細はこちら</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
