08cb8a8f9f5004bcefb1181acac5f62fc5edd24e
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2020-05-03 15:08:44 +0900

    style: lintの適用

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -30,7 +30,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [ '~/plugins/vuetify' ],
+  plugins: ['~/plugins/vuetify'],
   /*
    ** Nuxt.js dev-modules
    */
