1f8f6c5c41f21abcc6e478f74b66a6c69efbd08a
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2020-05-04 12:48:06 +0900

    feat: roomIdからfirestoreのドキュメントを取得するactionsの作成
    取得後の値を整形して取得するgetters関数も作成した

--- app/store/index.js
+++ app/store/index.js
@@ -1,10 +1,13 @@
 import Vuex from 'vuex'
 import { vuexfireMutations, firestoreAction } from 'vuexfire'
+import { db } from '~/plugins/firebase.js'
 
 const createStore = () => {
   return new Vuex.Store({
     state: {
-      rooms: []
+      rooms: [],
+      room: null,
+      isOwner: false
     },
     mutations: {
       ...vuexfireMutations
@@ -12,11 +15,30 @@ const createStore = () => {
     actions: {
       setRoomsRef: firestoreAction(({ bindFirestoreRef }, ref) => {
         bindFirestoreRef('rooms', ref)
+      }),
+      // roomIdからfirestoreのドキュメントを取得する関数
+      // todo: エラーハンドリング
+      setRoomRef: firestoreAction(async ({ bindFirestoreRef }, roomId) => {
+        const result = await bindFirestoreRef(
+          'room',
+          db.collection('rooms').doc(roomId)
+        )
       })
     },
     getters: {
       getRooms: (state) => {
         return state.rooms
+      },
+      getRoom: (state) => {
+        return state.room
+      },
+      // state.roomのowner, guestを配列化して取り出すgetter関数
+      getUsers: (state) => {
+        const users = [state.room.owner]
+        return users.concat(state.room.guest)
+      },
+      isStart: (state) => {
+        return state.room.isStart
       }
     }
   })
