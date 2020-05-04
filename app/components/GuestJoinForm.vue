791398d1a88beb94d1165636d609aba738a5a71a
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2020-05-04 12:47:27 +0900

    feat: ゲストの参加フォームのコンポーネントを作成

---
+++ app/components/GuestJoinForm.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-card light>
+    <v-card-title class="balck--text">
+      {{ roomName }}に参加します．
+    </v-card-title>
+    <v-card-text>
+      <v-text-field
+        v-model="formData.value"
+        :color="color"
+        :label="formData.label"
+      />
+      <v-btn :color="color" :disabled="submitDisabled" @click="handleClick"
+        >参加</v-btn
+      >
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    roomName: {
+      type: String,
+      default: 'test'
+    }
+  },
+  data() {
+    return {
+      color: 'info',
+      formData: {
+        label: '参加者名',
+        value: ''
+      }
+    }
+  },
+  computed: {
+    submitDisabled() {
+      return this.formData.value === ''
+    }
+  },
+  methods: {
+    handleClick() {
+      this.$emit('click', this.formData.value)
+    }
+  }
+}
+</script>
