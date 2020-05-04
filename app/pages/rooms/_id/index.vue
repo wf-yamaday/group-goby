8b3ddbc070b6c5306e6bf50cbc8155086e18efe9
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2020-05-04 12:49:21 +0900

    feat: roomの実装
    - fetchメソッドでvuexにfirestoreの値を入れる
    - 参加者の状況に応じてゲーム開始ボタンを制御
    - 主催者とゲストで表示するコンポーネントを分岐

--- app/pages/rooms/_id/index.vue
+++ app/pages/rooms/_id/index.vue
@@ -1,22 +1,61 @@
 <template>
   <div>
-    <room-name-and-link />
-    <member-list />
+    <room-name-and-link :name="room.name" :link="room.id" />
+    <member-list :users="users" />
+    <v-card v-if="isOwner" class="my-2" light>
+      <v-btn color="info" :disabled="!canStart" block>ゲームを開始</v-btn>
+    </v-card>
+    <v-card v-else class="my-2" light>
+      <v-btn color="info" block>準備完了</v-btn>
+    </v-card>
+    <v-dialog v-if="!isOwner" v-model="isFirst" persistent>
+      <guest-join-form :room-name="room.name" @click="joinRoom" />
+    </v-dialog>
+    <div class="black--text">
+      <!-- {{ room }} -->
+    </div>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapState } from 'vuex'
+
 import RoomNameAndLink from '~/components/RoomNameAndLink'
 import MemberList from '~/components/MemberList'
+import GuestJoinForm from '~/components/GuestJoinForm'
 
 export default {
   components: {
     RoomNameAndLink,
-    MemberList
+    MemberList,
+    GuestJoinForm
   },
-  fetch({ _store, _route }) {
-    // const id = route.params.id
-    // todo: idを使ってfirebaseとやりとり
+  async fetch({ store, route }) {
+    await store.dispatch('setRoomRef', route.params.id)
+  },
+  data() {
+    return {
+      isFirst: true
+    }
+  },
+  computed: {
+    ...mapGetters({ room: 'getRoom', users: 'getUsers' }),
+    ...mapState(['isOwner']),
+    canStart() {
+      if (this.users.length < 2) {
+        // 参加者が足りなければ始められない
+        return false
+      } else {
+        // 一人でもisReadyがfalseなら始められない
+        return !this.users.some((user) => user.isReady === false)
+      }
+    }
+  },
+  methods: {
+    joinRoom(guestName) {
+      // todo: firestoreにjoinするuserの情報を反映
+      this.isFirst = false
+    }
   }
 }
 </script>
