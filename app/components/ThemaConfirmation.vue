e74a658b9022de88b37d61c03b319c64d38b9242
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2020-05-04 20:42:37 +0900

    refactor: v-dialogの制御をcomponentsに委ねる

--- app/components/ThemaConfirmation.vue
+++ app/components/ThemaConfirmation.vue
@@ -1,16 +1,18 @@
 <template>
-  <v-card light :loading="loadingColor" :disabled="isLoading">
-    <v-card-title class="justify-center">今回のお題</v-card-title>
-    <v-card-text>
-      <p class="display-1 text-center">{{ thema }}</p>
-      <v-btn v-show="!isLoading" :color="color" outlined block @click="click"
-        >確認完了</v-btn
-      >
-      <p class="overline text-center mt-2">
-        参加者全員がお題を確認したらゲームを開始します．
-      </p>
-    </v-card-text>
-  </v-card>
+  <v-dialog v-model="isShow" persistent>
+    <v-card light :loading="loadingColor" :disabled="isLoading">
+      <v-card-title class="justify-center">今回のお題</v-card-title>
+      <v-card-text>
+        <p class="display-1 text-center">{{ thema }}</p>
+        <v-btn v-show="!isLoading" :color="color" outlined block @click="click"
+          >確認完了</v-btn
+        >
+        <p class="overline text-center mt-2">
+          参加者全員がお題を確認したらゲームを開始します．
+        </p>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
@@ -23,6 +25,7 @@ export default {
   },
   data() {
     return {
+      isShow: true,
       isLoading: false,
       color: 'info'
     }
