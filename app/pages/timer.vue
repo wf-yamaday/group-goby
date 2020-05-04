9fa91a17f7721715912f198715bb42aa0f8084a8
keychi25 <e165412y@st.u-gakugei.ac.jp>
2020-05-04 12:49:50 +0900

    add : TimerSample

---
+++ app/pages/timer.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-layout column justify-center align-center>
+    <h1>Sample Timer</h1>
+    <timer class="ma-2" />
+  </v-layout>
+</template>
+
+<script>
+import Timer from '~/components/Timer'
+
+export default {
+  components: {
+    Timer
+  }
+}
+</script>
+
+<style></style>
