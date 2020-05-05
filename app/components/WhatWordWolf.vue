d1bd90696d518a19528f5fed74ab10511d8a470f
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2020-05-05 17:55:01 +0900

    style: 余白を調整

--- app/components/WhatWordWolf.vue
+++ app/components/WhatWordWolf.vue
@@ -1,16 +1,17 @@
 <template>
   <v-card width="360" light>
-    <div class="d-flex flex-no-wrap justify-space-between">
-      <div>
-        <v-img width="150" height="180" src="/wolfman.png" />
-      </div>
-      <div>
-        <v-card-title class="balck--text">Word Wolfとは？</v-card-title>
-        <span>
-          「異なるお題」を与えられた人（Wolf）を探し出すゲームです．
-          <br />
+    <p class="headline text-center ma-0 pa-0 pt-2">Word Wolfとは？</p>
+    <div class="text-center">
+      <img width="150" height="180" src="/wolfman.png" />
+      <p class="subtitile-1 ma-0 pa-0 px-4 mb-2">
+        &#8220;お題が異なる一人を見つけましょう.&#8221;
+      </p>
+      <div class="text-left body grey--text text--darken-3 mx-4">
+        <p class="ma-0 pa-0 mb-2">
+          Word
+          Wolfは「異なるお題」を与えられた人（Wolf）を探し出すゲームです．<br />
           最低3人から始めることができます．
-        </span>
+        </p>
       </div>
     </div>
     <v-card-actions>
