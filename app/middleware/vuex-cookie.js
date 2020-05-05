9b0a63ecc6f09f4753b11ad10cab9a93d567c209
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2020-05-05 23:14:27 +0900

    feat: ページを再読み込みしても大丈夫なようにCookieを使用

--- app/middleware/vuex-cookie.js
+++ app/middleware/vuex-cookie.js
@@ -1,21 +1,39 @@
 import Cookies from 'universal-cookie'
 
-export default ({ route, _store, _redirect, error }) => {
+export default ({ route, store, _redirect, error }) => {
   // 無効なリクエスト
   if (!(route.params && route.params.id && route.params.id.length === 20)) {
-    return error({
-      statusCode: '400',
-      message: '無効なリクエストです'
-    })
+    return error(badRequest)
   }
 
   const cookies = new Cookies()
+  const roomId = cookies.get('roomId')
   const user = cookies.get('user')
+
+  // roomIDがない場合
+  if (!roomId) {
+    return
+  }
+
+  // roomIdがパスと一致しない場合
+  if (roomId !== route.params.id) {
+    cookies.remove('roomId')
+    return
+  }
 
   // cookieにuser情報がない場合
   if (!(user && user.id)) {
-    console.log('[debug]:user情報ないよ')
-    return
+    return error(badRequest)
   }
-  console.log('[debug]:通過')
+
+  // vuexの状態を更新
+  if (user.isOwner) {
+    store.commit('setIsOwner')
+  }
+  store.commit('setUserId', user.id)
+}
+
+const badRequest = {
+  statusCode: '400',
+  message: '無効なリクエストです'
 }
