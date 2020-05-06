8ac5eb104d2627766c968123bc72a0f2c351c2b8
keychi25 <e165412y@st.u-gakugei.ac.jp>
2020-05-06 18:07:04 +0900

    fix : timer/1To3

--- app/components/Timer.vue
+++ app/components/Timer.vue
@@ -12,9 +12,9 @@ export default {
   data() {
     return {
       timerId: '',
-      timer: 60,
-      duration: 60,
-      minutes: 1,
+      timer: 180,
+      duration: 180,
+      minutes: 3,
       seconds: 0,
       isRunning: false
     }
@@ -52,8 +52,8 @@ export default {
     },
     resetTimer() {
       this.isRunning = false
-      this.timer = 60 // 3min default
-      this.duration = 60
+      this.timer = 180 // 3min default
+      this.duration = 180
       this.minutes = parseInt(this.timer / 60, 10)
       this.seconds = parseInt(this.timer % 60, 10)
       this.minutes = this.minutes < 10 ? '0' + this.minutes : this.minutes
